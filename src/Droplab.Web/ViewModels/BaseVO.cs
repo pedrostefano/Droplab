45524882334c404e77f0e39b99c717dded5da80a
pedrostefano <pedrostefano03@gmail.com>
2017-08-15 00:11:08 -0300

    ViewModels

---
+++ src/Droplab.Web/ViewModels/BaseVO.cs
@@ -0,0 +1,8 @@
+namespace Droplab.Web.ViewModels
+{
+    public class BaseVO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ src/Droplab.Web/ViewModels/OrderVO.cs
@@ -0,0 +1,7 @@
+namespace Droplab.Web.ViewModels
+{
+    public class OrderVO : BaseVO
+    {
+        
+    }
+}
