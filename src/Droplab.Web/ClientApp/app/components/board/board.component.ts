1b387519f5caafb6289fe698b288d5027b415248
pedrostefano <pedrostefano03@gmail.com>
2017-08-24 22:44:36 -0300

    Alterada NavBar e adicionado BoardComponet

---
+++ src/Droplab.Web/ClientApp/app/components/board/board.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'board',
+    templateUrl: 'board.component.html'
+})
+
+export class BoardComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
